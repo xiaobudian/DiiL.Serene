56030b52849250ad55a6ce3c9e911110bec72b8b
刘帅 <849351660@qq.com>
2016-06-03 18:31:16 +0800

    创建AOC数据库

--- DiiL.Serene/DiiL.Serene.Web/App_Start/SiteInitialization.Migrations.cs
+++ DiiL.Serene/DiiL.Serene.Web/App_Start/SiteInitialization.Migrations.cs
@@ -11,7 +11,7 @@
 
     public static partial class SiteInitialization
     {
-        private static string[] databaseKeys = new[] { "Default", "Northwind" };
+        private static string[] databaseKeys = new[] { "Default", "Northwind", "Aoc" };
 
         /// <summary>
         /// Automatically creates a database for the template if it doesn't already exists.
@@ -110,12 +110,12 @@
 
             // safety check to ensure that we are not modifying an arbitrary database.
             // remove these two lines if you want DiiL.Serene migrations to run on your DB.
-            if (cs.ConnectionString.IndexOf(typeof(SiteInitialization).Namespace +
-                    @"_" + databaseKey + "_v1", StringComparison.OrdinalIgnoreCase) < 0)
-            {
-                SkippedMigrations = true;
-                return;
-            }
+            //if (cs.ConnectionString.IndexOf(typeof(SiteInitialization).Namespace +
+            //        @"_" + databaseKey + "_v1", StringComparison.OrdinalIgnoreCase) < 0)
+            //{
+            //    SkippedMigrations = true;
+            //    return;
+            //}
 
             string databaseType = "SqlServer";
             if (String.Equals(cs.ProviderName, "npgsql", StringComparison.OrdinalIgnoreCase))
---
+++ DiiL.Serene/DiiL.Serene.Web/Modules/Common/Migrations/AocDB/AocIPDB_20160603_170400_Initial.cs
@@ -0,0 +1,38 @@
+﻿using FluentMigrator;
+
+namespace DiiL.Serene.Migrations.AocDB
+{
+    [Migration(20160603170400)]
+    public class AocIPDB_20160603_170400_Initial : Migration
+    {
+        public override void Up()
+        {
+            IfDatabase("sqlserver")
+                .Execute.EmbeddedScript("DiiL.Serene.Modules.Common.Migrations.AocDB.AocDBScript_SqlServer.sql");
+
+            //IfDatabase("postgres")
+            //    .Execute.EmbeddedScript("DiiL.AocIP.Modules.Common.Migrations.NorthwindDB.NorthwindDBScript_Postgres.sql");
+
+            //IfDatabase("postgres")
+            //    .Execute.EmbeddedScript("DiiL.AocIP.Modules.Common.Migrations.NorthwindDB.NorthwindDBScript_PostgresData.sql");
+
+            //IfDatabase("mysql")
+            //    .Execute.EmbeddedScript("DiiL.AocIP.Modules.Common.Migrations.NorthwindDB.NorthwindDBScript_MySql.sql");
+
+            //IfDatabase("sqlserver", "postgres")
+            //    .Alter.Table("Customers")
+            //        .AddColumn("ID").AsInt32().Identity().NotNullable();
+
+            //IfDatabase("sqlserver", "postgres")
+            //    .Alter.Table("Territories")
+            //        .AddColumn("ID").AsInt32().Identity().NotNullable();
+
+            //Alter.Table("Products")
+            //    .AddColumn("ProductImage").AsString(100).Nullable();
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
